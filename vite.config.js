ee927ed0a93bc8d39bb7c999cb51e8a68c3a6ded
Tsuichi91 <Tsukatchi@gmail.com>
2026-09-09 20:35:25 +0200

    Include Chapter Detail page in Vite build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ command }) => ({
       input: {
         main: resolve(__dirname, 'index.html'),
         story: resolve(__dirname, 'story.html'),
+        chapter: resolve(__dirname, 'chapter.html'),
         archive: resolve(__dirname, 'archive.html')
       }
     }
