b78b72bf70360e4cfe9a93fd3a34a73481b39568
Tsuichi91 <Tsukatchi@gmail.com>
2026-09-10 06:23:03 +0200

    Add World page to Vite build

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig(({ command }) => ({
         main: resolve(__dirname, 'index.html'),
         story: resolve(__dirname, 'story.html'),
         chapter: resolve(__dirname, 'chapter.html'),
-        archive: resolve(__dirname, 'archive.html')
+        archive: resolve(__dirname, 'archive.html'),
+        world: resolve(__dirname, 'world.html')
       }
     }
   }
